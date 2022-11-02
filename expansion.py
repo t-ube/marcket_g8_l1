2f30dd2b23cf462940986b69daa6a3400e33de9b
Tomoyuki UBE <ube@coder.okinawa.jp>
2022-11-02 16:39:44 +0900

    first commit

---
+++ expansion.py
@@ -0,0 +1,18 @@
+def getList():
+    expansionList = [
+        'SA',
+        'S1W',
+        'S1H',
+        'S1a',
+        'S2',
+        'SP1',
+        'S2a',
+        'SC',
+        'S3',
+        'SD',
+        'S3a',
+        'S4',
+        'SP2',
+        'S4a'
+    ]
+    return expansionList
